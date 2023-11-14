a4fd66f216e41842127842290a2d2fd34e79e85a
Binary-tech001 <rafiumubarak2019@gmail.com>
2023-11-14 00:12:49 -0800

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import { Routes, Route } from 'react-router-dom'
+
+import './globals.css'
+
+function App() {
+
+  return (
+    <>
+     <p className='text-3xl underline'> App is here </p>
+    </>
+  )
+}
+
+export default App
