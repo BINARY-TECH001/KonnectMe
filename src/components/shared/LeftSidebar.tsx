b3cbc563d230a39ce9fc745abfcb1af5936fc0ce
Binary-tech001 <rafiumubarak2019@gmail.com>
2023-11-15 22:34:13 +0100

    Added Home layout

--- src/components/shared/LeftSidebar.tsx
+++ src/components/shared/LeftSidebar.tsx
@@ -1,13 +1,61 @@
-import { Link } from "react-router-dom"
+import { Link, NavLink, useNavigate } from "react-router-dom"
+import { Button } from "../ui"
+import { useSignOutAccount } from "@/lib/react-query/queriesAndMutations"
+import { useEffect } from "react";
+import { useUserContext } from "@/context/AuthContext";
+import { sidebarLinks } from "@/constants";
+import { INavLink } from "@/types";
 
 
 const LeftSidebar = () => {
+
+    const navigate = useNavigate();
+    const { mutate: signOut, isSuccess } = useSignOutAccount();
+
+    const { user } = useUserContext()
+
+    useEffect(()=> {
+        if(isSuccess){
+            navigate(0)
+        }
+    }, [isSuccess])
+
     return (
         <nav className="leftsidebar">
             <div className="flex flex-col gap-11">
             <Link to='/' className="flex gap-3 items-center">
                     <img src="/assets/images/logo.svg" alt="logo" width={170} height={36}/>
-                </Link>
+            </Link>
+
+            <Link to={`/profile/${user.id}`} className="flex gap-3 items-center">
+                <img src={user.imageUrl || '/assets/images/profile.png'} alt="profile" className="h-14 w-15 rounded-full"/>
+
+                <div className="flex flex-col">
+                    <p className="body-bold">
+                        {user.name}
+                    </p>
+                    <p className="small-regular text-light-3">
+                       @{user.username} 
+                    </p>
+                </div>
+            </Link>
+
+            <ul className="flex flex-col gap-6">
+                {sidebarLinks.map((link: INavLink) => {
+                    return (
+                        <li className="leftsidebar-link" key={link.label}>
+                        <NavLink to={link.route} className='flex gap-4 items-center p-4'>
+                            <img 
+                                src={link.imgURL}
+                                alt={link.label} 
+                                className="group-hover:invert-white"
+                            />
+                            {link.label}
+                        </NavLink>
+                        </li>
+                    )
+                })}
+            </ul>
             </div>
         </nav>
     )
