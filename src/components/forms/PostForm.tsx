10293fc7fc076af3f7b8f7d32568e3dca1bdde1f
Binary-tech001 <rafiumubarak2019@gmail.com>
2023-11-16 06:45:44 +0100

    Started the create post page UI and FileUploader

--- src/components/forms/PostForm.tsx
+++ src/components/forms/PostForm.tsx
@@ -15,36 +15,37 @@ import {
   Input,
   Textarea,
 } from "@/components/ui";
-// import { PostValidation } from "@/lib/validation";
-import { useToast } from "@/components/ui/use-toast";
-import { useUserContext } from "@/context/AuthContext";
-// import { FileUploader, Loader } from "@/components/shared";
-// import { useCreatePost, useUpdatePost } from "@/lib/react-query/queries";
+import FileUploader from "../shared/FileUploader";
 
-type PostFormProps = {
-  post?: Models.Document;
-  action: "Create" | "Update";
-};
 
-const PostForm = ({ post, action }: PostFormProps) => {
-  const navigate = useNavigate();
-  const { toast } = useToast();
-  const { user } = useUserContext();
-  const form = useForm<z.infer<typeof PostValidation>>({
-    resolver: zodResolver(PostValidation),
-    defaultValues: {
-      caption: post ? post?.caption : "",
-      file: [],
-      location: post ? post.location : "",
-      tags: post ? post.tags.join(",") : "",
-    },
-  });
+const PostForm = () => {
+    const navigate = useNavigate()
+    const formSchema = z.object({
+        username: z.string().min(2, {
+          message: "Username must be at least 2 characters.",
+        }),
+      })
+       
+        // 1. Define your form.
+        const form = useForm<z.infer<typeof formSchema>>({
+          resolver: zodResolver(formSchema),
+          defaultValues: {
+            username: "",
+          },
+        })
+       
+        // 2. Define a submit handler.
+        function onSubmit(values: z.infer<typeof formSchema>) {
+          // Do something with the form values.
+          // ✅ This will be type-safe and validated.
+          console.log(values)
+        }
 
 
   return (
     <Form {...form}>
       <form
-        onSubmit={form.handleSubmit()}
+        onSubmit={form.handleSubmit(onSubmit)}
         className="flex flex-col gap-9 w-full  max-w-5xl">
         <FormField
           control={form.control}
@@ -71,8 +72,6 @@ const PostForm = ({ post, action }: PostFormProps) => {
               <FormLabel className="shad-form_label">Add Photos</FormLabel>
               <FormControl>
                 <FileUploader
-                  fieldChange={field.onChange}
-                  mediaUrl={post?.imageUrl}
                 />
               </FormControl>
               <FormMessage className="shad-form_message" />
@@ -121,6 +120,11 @@ const PostForm = ({ post, action }: PostFormProps) => {
             className="shad-button_dark_4"
             onClick={() => navigate(-1)}>
             Cancel
+          </Button>
+          <Button
+            type="submit"
+            className="shad-button_primary whitespace-nowrap">
+            Post
           </Button>
         </div>
       </form>
