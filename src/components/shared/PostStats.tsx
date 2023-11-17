38bfa44bb889cb72d9a94844458bd4a9839a7096
Binary-tech001 <rafiumubarak2019@gmail.com>
2023-11-17 19:51:38 +0100

    Home UI | Likes fnx done

--- src/components/shared/PostStats.tsx
+++ src/components/shared/PostStats.tsx
@@ -1,4 +1,9 @@
+import React, { useState } from "react";
+import { useDeleteSavedPost, useGetCurrentUser, useLikePost, useSavePost } from "@/lib/react-query/queriesAndMutations";
 import { Models } from "appwrite"
+import { checkIsLiked } from "@/lib/utils";
+import { set } from "zod";
+import { deleteSavedPost } from "@/lib/appwrite/api";
 
 type PostStatsProps ={
     post: Models.Document;
@@ -6,27 +11,72 @@ type PostStatsProps ={
 }
 
 const PostStats = ({ post, userId } : PostStatsProps) => {
+    const likesList = post.likes.map((user: Models.Document) => user.$id)
+
+    const [likes, setLikes] = useState(likesList)
+    const [isSaved, setIsSaved] = useState(false)
+
+    const { mutate: likePost } = useLikePost()
+    const { mutate: savePost } = useSavePost()
+    const { mutate: deletePost } = useDeleteSavedPost()
+
+    const { data: currentUser } = useGetCurrentUser()
+
+    const handleLikePost = (e: React.MouseEvent) => {
+        e.stopPropagation();
+
+        let newLikes = [...likes]
+        const hasLiked = newLikes.includes(userId)
+
+        if(hasLiked){
+            newLikes = newLikes.filter((id) => id !== userId)
+        } else {
+            newLikes.push(userId)
+        }
+        setLikes(newLikes)
+        likePost({ postId: post.$id, likesArray:newLikes })
+    }
+
+    const handleSavePost = (e: React.MouseEvent)=> {
+        e.stopPropagation();
+        const savedPostRecord = currentUser?.save.find((record: Models.Document) => record.$id === post.$id)
+        if(savedPostRecord){
+            setIsSaved(false)
+            deleteSavedPost(savedPostRecord.$id)
+        } else{
+            savePost({ postId: post.$id, userId })
+            setIsSaved(true)
+
+        }
+
+    }
   return (
     <div className="flex justify-between z-20 mt-5">
       <div className="flex gap-2 mr-5">
         <img 
-            src="/assets/icons/like.svg" 
+            src={checkIsLiked(likes, userId) 
+                ? "/assets/icons/liked.svg"
+                : "/assets/icons/like.svg"
+            }
             alt="like" 
             width={20}
             height={20}
-            onClick={()=> {}}
+            onClick={handleLikePost}
             className="cursor-poinnter"
         />
-        <p className="small-medium lg:base-medium">0</p>
+        <p className="small-medium lg:base-medium">{likes.length}</p>
       </div>
 
       <div className="flex gap-2">
         <img 
-            src="/assets/icons/save.svg" 
+            src={isSaved 
+                ? "/assets/icons/saved.svg"
+                : "/assets/icons/save.svg"
+            }
             alt="like" 
             width={20}
             height={20}
-            onClick={()=> {}}
+            onClick={handleSavePost}
             className="cursor-poinnter"
         />
       </div>
