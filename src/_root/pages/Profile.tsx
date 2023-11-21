5ddbd222d844c6ef0b377d66d7f215daa0aed205
Binary-tech001 <rafiumubarak2019@gmail.com>
2023-11-21 18:28:05 +0100

    Profile UI

--- src/_root/pages/Profile.tsx
+++ src/_root/pages/Profile.tsx
@@ -3,36 +3,46 @@ import { Button } from "@/components/ui"
 
 const Profile = () => {
   return (
-    <div className="container_profile">
-    <div className="flex items-center justify-center w-full">
-      <div className="image">
-        <img src="/assets/images/profile.png" alt="user" />
+    <div className="container_profile flex-col justify-center flex-1 items-start mt-20">
+    <div className="flex justify-center flex-col w-full gap-6">
+    <div className="image h-[150px] w-[150px]">
+        <img src="/assets/images/profile.png" alt="user" className="w-full"/>
       </div>
 
-      <div className="userInfo">
-        <h3> Binary Tech </h3>
-        <p> @BinaryTech001 </p>
+      <div className="userInfo flex flex-col gap-1">
+        <h3 className="h3-bold md:h2-bold text-left w-full"> Binary Tech </h3>
+        <p className="text-[#7878A3] font-[18px]"> @BinaryTech001 </p>
+
+        <div className="postInfo flex gap-3">
         <div className="post">
-          <span> 978 </span>
-          <span> Posts </span>
+          <span className="font-[20px] text-[#877EFF]"> 978 </span>
+          <span className="text-sm"> Posts </span>
         </div>
         <div className="followers">
-          <span> 230k </span>
-          <span> Followers </span>
+          <span className="font-[20px] text-[#877EFF]"> 230k </span>
+          <span className="text-sm"> Followers </span>
         </div>
         <div className="following">
-          <span> 68 </span>
-          <span> Following </span>
+          <span className="font-[20px] text-[#877EFF]"> 68 </span>
+          <span className="text-sm"> Following </span>
+        </div>
         </div>
       </div>
 
-      <div className="actionBtn">
-        <Button>
+      <div className="actionBtn flex gap-3 items-start justify-start">
+        <Button className="bg-[#877EFF]">
           Follow
         </Button>
-        <Button>
+        <Button className="bg-[#EFEFEF] text-dark-1">
           Message
         </Button>
+      </div>
+    </div>
+
+    <div className="flex relative">
+      <div className="flex relative">
+        hello
+
       </div>
     </div>
     </div>
