e3076a1859f13471d512a871112a36a5fda9a01f
Binary-tech001 <rafiumubarak2019@gmail.com>
2023-11-25 17:44:04 +0100

    All users UI

--- src/_root/pages/AllUsers.tsx
+++ src/_root/pages/AllUsers.tsx
@@ -1,17 +1,41 @@
+import TopCreator from '@/components/shared/TopCreator'
 import React from 'react'
+import { useGetUsers } from '@/lib/react-query/queriesAndMutations'
+import { Models } from 'appwrite'
+import UserCard from '@/components/shared/userCard'
+import { Loader } from 'lucide-react'
 
 const AllUsers = () => {
+  const { data: creators, isFetching: isUserLoading, isError: isErrorCreators} = useGetUsers();
+
   return (
     <div className='explore-container mt-3'>
-       <div className="flex-between w-full max-w-5xl mt-2 mb-2">
+      <div className="w-full max-w-5xl mt-2 mb-2">
         <div className="flex gap-2">
           <img src="/assets/icons/people.svg" alt="" className='invert-white' width={28} height={28} />
-        <h3 className="body-bold md:h3-bold">All Users</h3>
+          <h3 className="body-bold md:h3-bold">All Users</h3>
+        </div>
 
+        <div className='mt-10 p-5 lg:block'>
+          <div className="user-container">
+            {
+              isUserLoading && !creators ? (
+                <Loader />
+              ) : (
+                <div className="grid 2xl:grid-cols-3 gap-6">
+                  {
+                    creators?.documents.map((creator: Models.Document) => (
+                      <UserCard user={creator} key={creator.name} />
+                    ))
+                  }
+                </div>
+              )
+            }
+          </div>
         </div>
         </div>
-    </div>
-  )
+      </div>
+      )
 }
 
-export default AllUsers
+      export default AllUsers
