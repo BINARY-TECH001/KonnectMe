a63c598466bc6f60190982cd40fa0ee1840277ce
Binary-tech001 <rafiumubarak2019@gmail.com>
2023-11-25 17:36:30 +0100

    All users UI

--- src/_root/pages/AllUsers.tsx
+++ src/_root/pages/AllUsers.tsx
@@ -2,8 +2,14 @@ import React from 'react'
 
 const AllUsers = () => {
   return (
-    <div>
-      AllUsers
+    <div className='explore-container mt-3'>
+       <div className="flex-between w-full max-w-5xl mt-2 mb-2">
+        <div className="flex gap-2">
+          <img src="/assets/icons/people.svg" alt="" className='invert-white' width={28} height={28} />
+        <h3 className="body-bold md:h3-bold">All Users</h3>
+
+        </div>
+        </div>
     </div>
   )
 }
